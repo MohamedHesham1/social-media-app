8c6164480688464766314f9db44b3f58efd06ef4
mohamed <mohamed.hesham.2049@gmail.com>
2023-02-21 17:22:29 +0200

    feat: :sparkles: add Post component

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,59 @@
+import './post.scss';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Link } from 'react-router-dom';
+import Comments from '../comments/Comments';
+import { useState } from 'react';
+
+const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
+
+  //TEMPORARY
+  const liked = false;
+
+  return (
+    <li className='post'>
+      <div className='container'>
+        <div className='user'>
+          <div className='userInfo'>
+            <img src={post.profilePic} alt='user profile' />
+            <div className='details'>
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <span className='name'>{post.name}</span>
+              </Link>
+              <span className='date'>1 min ago</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className='content'>
+          <p>{post.desc}</p>
+          <img src={post.img} alt='attachment' />
+        </div>
+        <div className='info'>
+          <div className='item'>
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            12 Likes
+          </div>
+          <div className='item' onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon />
+            12 Comments
+          </div>
+          <div className='item'>
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+        {commentOpen && <Comments />}
+      </div>
+    </li>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,3 +1,4 @@
+import Post from '../post/Post';
 import './posts.scss';
 
 const Posts = () => {
