fe3d0781c9d8ef34e04b05332f05259594e96934
mohamed <mohamed.hesham.2049@gmail.com>
2023-02-25 15:08:52 +0200

    feat: :sparkles: add back-end files

---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './register.scss';
+import axios from 'axios';
+
+function Register() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:8800/api/auth/register', inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err);
+
+  return (
+    <div className='register'>
+      <div className='card'>
+        <div className='left'>
+          <h1>Vivid Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, c onsectetur adipisicing elit. A aut
+            consectetur adipisci velit ratione laboriosam beatae. Eligendi
+            quisquam, tempore quae modi voluptate libero ab dignissimos illum
+            cupiditate nesciunt quis non.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to='/login'>
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className='right'>
+          <h1>Register </h1>
+          <form action=''>
+            <input
+              type='text'
+              placeholder='Username'
+              name='username'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              placeholder='Email'
+              name='email'
+              onChange={handleChange}
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              onChange={handleChange}
+            />
+            {/* {err && err} */}
+            <button onClick={handleClick}>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
