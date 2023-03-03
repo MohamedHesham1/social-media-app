d208845c95942efc52dca8d6e65f34976146aa06
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-03-03 22:37:44 +0200

    fix: :bug: change baseURL

--- api/index.js
+++ api/index.js
@@ -1,31 +1,31 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import authRoutes from './routes/auth.js';
-import userRoutes from './routes/users.js';
-import postRoutes from './routes/posts.js';
-import commentRoutes from './routes/comments.js';
-import likeRoutes from './routes/likes.js';
-import relationshipRoutes from './routes/relationships.js';
-import cors from 'cors';
-import multer from 'multer';
-import cookieParser from 'cookie-parser';
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
+import cors from "cors";
+import multer from "multer";
+import cookieParser from "cookie-parser";
 
 //middlewares
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "https://social-media-frontend-dbtc.onrender.com",
   })
 );
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../client/public/upload');
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
@@ -34,18 +34,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.post('/api/upload', upload.single('file'), (req, res) => {
+app.post("/api/upload", upload.single("file"), (req, res) => {
   const file = req.file;
   res.status(200).json(file.filename);
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/likes', likeRoutes);
-app.use('/api/relationships', relationshipRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
 app.listen(8800, () => {
-  console.log('API working!');
+  console.log("API working!");
 });
--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://social-media-backend-z94r.onrender.com/",
   withCredentials: true,
 });
