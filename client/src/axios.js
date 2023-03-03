f5fb34cd49b27d2c20f29b704a62da54ee1b9f28
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-03-04 01:00:48 +0200

    fix: :bug: correct baseUrl

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://social-media-backend-z94r.onrender.com/",
+  baseURL: "https://social-media-backend-z94r.onrender.com",
   withCredentials: true,
 });
